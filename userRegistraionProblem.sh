b74fa224c68aaeae7b59f051241044ae30bdbd8f
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-08 17:35:30 +0530

    Valid Lastname

--- userRegistraionProblem.sh
+++ userRegistraionProblem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
+#firstname
 read -p "Enter firstName: " value
 firstName="^[ A-Z ]{1}[a-z]{2,}$"
 if [[ $value =~ $firstName ]]
@@ -8,4 +8,12 @@ then
 else
    echo "False"
 fi
-
+#lastname
+read -p "Enter lastName: " value2
+lastName="^[ A-Z ]{1}[a-z]{2,}$"
+if [[ $value2 =~ $lastName ]]
+then
+   echo "True"
+else
+   echo "False"
+fi
