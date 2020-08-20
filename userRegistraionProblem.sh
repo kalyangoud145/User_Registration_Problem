c1f9afd12b938951cd8202d8580769a56d944aa9
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-20 09:58:40 +0530

    Added functions

--- userRegistraionProblem.sh
+++ userRegistraionProblem.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 #firstname
+function firstName(){
 read -p "Enter firstName: " value
 firstName="^[ A-Z ]{1}[a-z]{2,}$"
 if [[ $value =~ $firstName ]]
@@ -8,7 +9,9 @@ then
 else
    echo "False"
 fi
+}
 #lastname
+function lastName(){
 read -p "Enter lastName: " value
 lastName="^[ A-Z ]{1}[a-z]{2,}$"
 if [[ $value =~ $lastName ]]
@@ -17,7 +20,9 @@ then
 else
    echo "False"
 fi
+}
 #email validation
+function emailValidation(){
 read -p "Enter email: " value
 pattern="^([a-zA-Z0-9\_\-\+\-]{1}[a-zA-Z0-9\_\-\.\+\-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,}|[0-9]{1,})(\]?)$"
 if [[ $value =~ $pattern ]]
@@ -26,7 +31,9 @@ then
 else
    echo "False"
 fi
+}
 #Mobile N.O
+function mobileNumberFormat(){
 read -p "mobile Number: " value
 mobileNumber="^[0-9]{2}[: :][0-9]{10}$"
 if [[ $value =~ $mobileNumber ]]
@@ -35,7 +42,9 @@ then
 else
    echo "False"
 fi
+}
 #Password Validation
+function passwordValidation(){
 read -p "Enter password " password
 if [[ ${#password} -ge 8 && "$password" == *[[:upper:]]* && "$password" == *[[:lower:]]* && "$password" == *[0-9]* &&"$password" == *[[@#%^*]]{1}* ]]
 then
@@ -43,13 +52,13 @@ then
 else
    echo "False"
 fi
-#email Id pattern for clearing Test samples 
-read value
-pattern="^([a-zA-Z0-9\_\-\+\-]{1}[a-zA-Z0-9\_\-\.\+\-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,}|[0-9]>
-if [[ $value =~ $pattern ]]
-then
-   echo "True"
-else
-   echo "False"
-fi
+}
 
+function main(){
+	firstName
+	lastName
+	emailValidation
+	mobileNumberFormat
+	passwordValidation
+}
+main
